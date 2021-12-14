9a450c496038691e3e784a98c7cf5bf9dcbce22e
maaverik <nithinjosetom@gmail.com>
2021-12-14 21:52:25 +0530

    Set up required setting changes for dependencies and db

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'admin',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
