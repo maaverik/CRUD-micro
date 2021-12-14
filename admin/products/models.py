8032031bfb28fe05cf0a831f4e216f17dd87f0cf
maaverik <nithinjosetom@gmail.com>
2021-12-14 23:45:31 +0530

    Partially added product APIs and ORM logic

--- admin/products/models.py
+++ admin/products/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+
 class Product(models.Model):
     title = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
     likes = models.PositiveBigIntegerField(default=0)
+
 
 class User(models.Model):
     # has id by default, that's all we need
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):    # for /api/products route
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):    # for /api/products route
+        pass
+
+    def retrieve(self, request, pk=None):   # for /api/products/<id:str>
+        pass
+
+    def update(self, request, pk=None):   # for /api/products/<id:str>
+        pass
+
+    def destroy(self, request, pk=None):   # for /api/products/<id:str>
+        pass
