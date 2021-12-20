6125d493161abf2311293d72d568548f2d0f8f0f
maaverik <nithinjosetom@gmail.com>
2021-12-20 15:08:59 +0530

    Added flask route to list products and used dataclass to make Product JSON serializable

--- main/main.py
+++ main/main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from dataclasses import dataclass
+
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
@@ -9,7 +11,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@db/main"
 db = SQLAlchemy(app)
 
 
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     # autoincrement False because product will be created in admin app and sent here
     # from RabbitMQ, so id will be different
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
@@ -17,6 +24,7 @@ class Product(db.Model):
     image = db.Column(db.String(200))
 
 
+@dataclass
 class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
@@ -25,9 +33,9 @@ class ProductUser(db.Model):
     UniqueConstraint("user_id", "product_id", name="user_product_unique")
 
 
-@app.route("/")
+@app.route("/api/products")
 def index():
-    return "Hello"
+    return jsonify(Product.query.all())
 
 
 if __name__ == "__main__":
