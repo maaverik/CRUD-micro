06c530dfc1d9188b40dcf465a71c7a3402789ecc
maaverik <nithinjosetom@gmail.com>
2021-12-22 21:07:23 +0530

    Added frontend routes and formatted code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Products from "./admin/Products";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./main/Main";
+import ProductsCreate from "./admin/ProductsCreate";
+import ProductsEdit from "./admin/ProductsEdit";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/admin/products" element={<Products />} />
+          <Route path="/admin/products/create" element={<ProductsCreate />} />
+          <Route path="/admin/products/:id/edit" element={<ProductsEdit />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
